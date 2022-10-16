0b4b8942be15846a8633e6b0a90fb53b31155688
Bukrek35TR <fatih.yigit.35@gmail.com>
2022-10-16 17:27:03 +0300

    projecteuler.net

--- projecteuler.net_projects/ID9_SpecialPythagoreanTriplet/src/Main.java
+++ projecteuler.net_projects/ID9_SpecialPythagoreanTriplet/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		for (int a = 1; a < 1000; a++) {
+		for (int b = 1; b < 1000; b++) {
 
-			for (int b = 1; b < 1000; b++) {
+			for (int a = 1; a < b; a++) {
 				int c = (int) Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 				if ((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2) && a > 0 && b > a && c > b) {
 
