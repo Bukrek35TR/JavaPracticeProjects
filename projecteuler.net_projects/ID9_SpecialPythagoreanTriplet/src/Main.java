f45fd9d6f5c19181056bf2ef74ed0e5422587ea6
Bukrek35TR <fatih.yigit.35@gmail.com>
2022-10-16 17:32:57 +0300

    projecteuler.net

--- projecteuler.net_projects/ID9_SpecialPythagoreanTriplet/src/Main.java
+++ projecteuler.net_projects/ID9_SpecialPythagoreanTriplet/src/Main.java
@@ -5,20 +5,20 @@ public class Main {
 
 		for (int b = 1; b < 1000; b++) {
 
-			for (int a = 1; a < b; a++) {
+			for (int a = 1; a < 1000; a++) {
 				int c = (int) Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-				if ((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2) && a > 0 && b > a && c > b) {
 
-					if ((a + b + c) == 1000) {
-						System.out.println("a = " + a);
-						System.out.println("b = " + b);
-						System.out.println("c = " + c);
-						System.out.println("a+b+c = " + (a + b + c));
-						System.out.println("-------------------");
-						System.out.println("Conclusion (a*b*c) = " + (a * b * c));
-						return;
-					}
+				if ((a + b + c) == 1000 && (Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2) && a > 0 && b > a
+						&& c > b) {
+					System.out.println("a = " + a);
+					System.out.println("b = " + b);
+					System.out.println("c = " + c);
+					System.out.println("a+b+c = " + (a + b + c));
+					System.out.println("-------------------");
+					System.out.println("Conclusion (a*b*c) = " + (a * b * c));
+					return;
 				}
+
 			}
 		}
 	}
